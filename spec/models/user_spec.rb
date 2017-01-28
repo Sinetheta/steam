e79625f66c3cf2260f22b9c61701858ba282f6ec
Kevin Attfield <kevin@stembolt.com>
2017-01-28 15:06:36 -0800

    Fix email validation spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe User, :type => :model do
     it { should validate_presence_of :email }
     it { should allow_value('legit@email.com').for(:email) }
     it { should_not allow_value('noname.com').for(:email) }
-    it { should_not allow_value('no@tld').for(:email) }
+    it { should_not allow_value('nodomain@').for(:email) }
 
     it { should validate_presence_of :password }
     it { should validate_length_of :password }
